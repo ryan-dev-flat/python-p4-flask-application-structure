e631df6a31f7095e50e89552f01cff6d85410f8b
Ryan <coap.ryanmontgomery@gmail.com>
2024-08-26 10:25:38 -0600

    new2

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
     return '<h1>Welcome to my page!</h1>'
 
@@ -26,4 +26,4 @@ def user(username):
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
 
-    
+
